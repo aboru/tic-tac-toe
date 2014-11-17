78cb7e75d2719a8320d66f0a7a613151d238206a
Anders Borg <anders.borg@medianet.es>
2014-11-16 18:42:28 -0600

    Board creates and prints properly

---
+++ source/main.rkt
@@ -0,0 +1,17 @@
+#!/usr/bin/env racket
+#lang racket
+
+#|
+
+  main.rkt
+
+  Copyright (c) 2014 Anders Borg (see LICENSE for details)
+
+  This is the main file of the project. The main file is reponsible for loading
+  the project modules and directing the execution flow to the functions that we
+  desire.
+
+  Currently, main is awaiting development of some base modules before it can be
+  built properly.
+
+|#
