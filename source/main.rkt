22f065a3e2f9056a6ec0cf53305408659ee88809
Anders Borg <anders.borg@medianet.es>
2014-11-17 01:42:05 -0600

    Playable game with game over

--- source/main.rkt
+++ source/main.rkt
@@ -15,3 +15,23 @@
   built properly.
 
 |#
+
+(require
+  "io/input.rkt"
+  "game/utilities.rkt"
+  "board/board.rkt"
+  "board/board-generator.rkt")
+
+(define (game-loop board turn)
+  (turn-message turn board)
+  (let* ([moves (move board)]
+         [next (modify board (list-ref moves 0) (list-ref moves 1) (player-value turn))])
+    (if (continuation-check next turn)
+      (game-loop
+        next (change-turn turn))
+      (displayln "game over"))))
+
+(define (init-game)
+  (welcome)(game-loop (create-board) 1))
+
+(init-game)
